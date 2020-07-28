f9f55e3bf629edb53d4a90fa8559e1f00162ee1b
Gustavo Saboya <saboya.gustavo@gmail.com>
2020-07-28 19:34:15 -0300

    JavaScript basics: using localStorage API to add an username (not null) to the page.

--- scripts/main.js
+++ scripts/main.js
@@ -14,3 +14,22 @@ myImage.onclick = function() {
         myImage.setAttribute('src','./images/firefox-icon.png');
     }
 }
+
+let myButton = document.querySelector('button')
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+        setUserName();
+    } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}.`;
+    }
+}
+myButton.onclick = setUserName;
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = `Mozilla is cool, ${storedName}.`;
+}
